6aee76f4cddba16f96b19b1a00057e629bb8efe8
Gabriel Junio <gabriel.junio@rocketmail.com>
2024-01-31 20:23:25 -0300

    feat:Creating the launching force

---
+++ src/Assets/Scripts/Force Selection/Axios/AxiosController.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class AxiosController : MonoBehaviour
+{
+    private bool CanPush;
+    private double ForceToPush;
+
+    private Rigidbody2D _rigidbody2D;
+
+    private void OnEnable()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+
+        CanPush = false;
+
+        BarForce.OnForceSelected += BarForce_OnForceSelected;
+
+    }
+
+    private void BarForce_OnForceSelected( double forceSelected )
+    {
+        CanPush = true;
+        ForceToPush = forceSelected;
+    }
+
+    void Update()
+    {
+        if (CanPush)
+        {
+            _rigidbody2D.AddForce(new Vector2(1, 1) * (int) ForceToPush, ForceMode2D.Impulse);
+            CanPush = false;
+        }
+    }
+}
